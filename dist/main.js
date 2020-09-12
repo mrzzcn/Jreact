51d45a40e6c45622eac5bfa23c692435a2956918
zhangzhen <jack.zhang@aihuishou.com>
2020-09-12 22:05:39 +0800

    :sparkles: Custom Component

--- dist/main.js
+++ dist/main.js
@@ -86,43 +86,205 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./Jreact.js":
+/*!*******************!*\
+  !*** ./Jreact.js ***!
+  \*******************/
+/*! exports provided: Component, createElement, render */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Component", function() { return Component; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createElement", function() { return createElement; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var ElementWrapper = /*#__PURE__*/function () {
+  function ElementWrapper(type) {
+    _classCallCheck(this, ElementWrapper);
+
+    this.root = document.createElement(type);
+  }
+
+  _createClass(ElementWrapper, [{
+    key: "setAttribute",
+    value: function setAttribute(name, value) {
+      this.root.setAttribute(name, value);
+    }
+  }, {
+    key: "appendChild",
+    value: function appendChild(component) {
+      this.root.appendChild(component.root);
+    }
+  }]);
+
+  return ElementWrapper;
+}();
+
+var TextWrapper = function TextWrapper(content) {
+  _classCallCheck(this, TextWrapper);
+
+  this.root = document.createTextNode(content);
+};
+
+var Component = /*#__PURE__*/function () {
+  function Component() {
+    _classCallCheck(this, Component);
+
+    this.props = Object.create(null);
+    this.children = [];
+    this._root = null;
+  }
+
+  _createClass(Component, [{
+    key: "setAttribute",
+    value: function setAttribute(name, value) {
+      this.props[name] = value;
+    }
+  }, {
+    key: "appendChild",
+    value: function appendChild(component) {
+      this.children.push(component);
+    }
+  }, {
+    key: "root",
+    get: function get() {
+      if (!this._root) {
+        this._root = this.render().root;
+      }
+
+      return this._root;
+    }
+  }]);
+
+  return Component;
+}();
+function createElement(type, attributes) {
+  var element;
+
+  if (typeof type === 'string') {
+    element = new ElementWrapper(type);
+  } else {
+    element = new type();
+  }
+
+  for (var attr in attributes) {
+    element.setAttribute(attr, attributes[attr]);
+  }
+
+  var insertChildren = function insertChildren(children) {
+    var _iterator = _createForOfIteratorHelper(children),
+        _step;
+
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var child = _step.value;
+
+        if (typeof child === 'string') {
+          child = new TextWrapper(child);
+        }
+
+        if (Array.isArray(child)) {
+          insertChildren(child);
+        } else {
+          element.appendChild(child);
+        }
+      }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
+  };
+
+  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    children[_key - 2] = arguments[_key];
+  }
+
+  insertChildren(children);
+  return element;
+}
+function render(component, parentDom) {
+  console.log('rendering', component, parentDom);
+  parentDom.appendChild(component.root);
+}
+
+/***/ }),
+
 /***/ "./index.js":
 /*!******************!*\
   !*** ./index.js ***!
   \******************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Jreact__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Jreact */ "./Jreact.js");
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 
 console.log('Hello World');
-window.Jreact = {
-  createElement: function createElement(tagName, attributes) {
-    var element = document.createElement(tagName);
-
-    for (var attr in attributes) {
-      element.setAttribute(attr, attributes[attr]);
-    }
-
-    for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
-      children[_key - 2] = arguments[_key];
-    }
-
-    for (var _i = 0, _children = children; _i < _children.length; _i++) {
-      var child = _children[_i];
-
-      if (typeof child === 'string') {
-        child = document.createTextNode(child);
-      }
-
-      element.appendChild(child);
-    }
-
-    return element;
-  }
+var Jreact = {
+  createElement: _Jreact__WEBPACK_IMPORTED_MODULE_0__["createElement"]
 };
-document.body.appendChild(Jreact.createElement("div", {
+
+var MyComponent = /*#__PURE__*/function (_Component) {
+  _inherits(MyComponent, _Component);
+
+  var _super = _createSuper(MyComponent);
+
+  function MyComponent() {
+    _classCallCheck(this, MyComponent);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass(MyComponent, [{
+    key: "render",
+    value: function render() {
+      return Jreact.createElement("div", null, Jreact.createElement("h1", null, "My Component"), this.children);
+    }
+  }]);
+
+  return MyComponent;
+}(_Jreact__WEBPACK_IMPORTED_MODULE_0__["Component"]);
+
+Object(_Jreact__WEBPACK_IMPORTED_MODULE_0__["render"])(Jreact.createElement(MyComponent, {
   id: "root",
   "class": "class1"
-}, Jreact.createElement("div", null, "Hello World"), Jreact.createElement("div", null), Jreact.createElement("div", null)));
+}, Jreact.createElement("div", null, "Hello World"), Jreact.createElement("div", null), Jreact.createElement("div", null)), document.body);
 
 /***/ })
 
--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+import { createElement, render, Component } from './Jreact'
 console.log('Hello World');
 
-window.Jreact = {
-  createElement: (tagName, attributes, ...children) => {
-    const element = document.createElement(tagName);
-    for (const attr in attributes) {
-      element.setAttribute(attr, attributes[attr])
-    }
-    for (let child of children) {
-      if (typeof child === 'string') {
-        child = document.createTextNode(child)
-      }
-      element.appendChild(child)
-    }
-    return element;
+const Jreact = { createElement }
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Component</h1>
+        {this.children}
+      </div>
+    )
   }
 }
 
-document.body.appendChild(<div id="root" class="class1">
-  <div>Hello World</div>
-  <div></div>
-  <div></div>
-</div>)
+
+render(
+  <MyComponent id="root" class="class1">
+    <div>Hello World</div>
+    <div></div>
+    <div></div>
+  </MyComponent>,
+  document.body
+)
